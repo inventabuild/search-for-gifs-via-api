9130941c3590cb3d3809ba07d3b7520602f5bba0
inventabuild <mrabushka@hotmail.com>
2023-11-01 18:20:30 -0500

    Added trending gifs to starting home page, added footer, bug fix onm html js-gif-container section.

--- gif.js
+++ gif.js
@@ -1,9 +1,15 @@
 let gifContainer = document.querySelector('.js-gif-container');
 let form = document.querySelector('.js-gif-form');
 let repoInput = document.querySelector('[name=gif-name]');
+let gifDescription = document.querySelector(".gif-description-span");
+
+function loadTrendingGifsInJs(repoName) {
+    repoName=""
+    fetchRepo(repoName)
+}
 
 function fetchRepo(repoName) {
-    
+    if (repoName !== ""){
         fetch(`http://api.giphy.com/v1/gifs/search?q=${repoName}&api_key=${GIPHY_KEY}&limit=20`)
         .then(data => data.json())
         .then(response => {
@@ -19,22 +25,36 @@ function fetchRepo(repoName) {
                 x++
             });
                 gifContainer.innerHTML = html;
+                gifDescription.innerHTML = repoName + " GIFs ...";
         })
         .catch(err => {
             console.warn(err);
             gifContainer.innerHTML = `<p>Error fetching repo ${repoName}</p>`;    
         })
+    }
+    else{
+        fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_KEY}&limit=20`)
+        .then(data => data.json())
+        .then(response => {
+            // erroneous response is not an array, but an object
+            if (!Array.isArray(response.data)) { 
+                throw 'Erroneous response';
+            }
+            let html = ""
+            let x = 0
+            response.data.forEach(element => {
+                html += `<iframe src="${response.data[x].embed_url}" class="image-style"></iframe>`
+                x++
+            });
+                gifContainer.innerHTML = html;
+                gifDescription.innerHTML = "Trending GIFs ...";
+        })
         .catch(err => {
             console.warn(err);
             gifContainer.innerHTML = `<p>Error fetching repo ${repoName}</p>`;    
         })
-    
+    }
 }
-
-//From P5 YouTube tutorial
-// function gotData(data){
-//     println(data.data[0].images.original.url)
-// }
 
 function formSubmitted(event) {
     event.preventDefault();
